23ee84e116bfeb7bf8a80e692a849821cdb65231
Tyler O'Neil <tyleroneildev@gmail.com>
2024-12-20 23:51:26 -0500

    Fix mobile grid

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,35 @@ function App() {
   return (
     <div
       className='min-h-screen bg-gray-900 text-white grid gap-4 p-4
-                 sm:grid-cols-1 
+                 grid-cols-1 
                  md:grid-cols-2 
                  lg:grid-cols-6'
     >
       {/* Profile Card + GitHub */}
-      <div className='lg:col-span-2 lg:row-span-3 md:col-span-full flex flex-col gap-4'>
-        <div className='flex-1'>
+      <div className='lg:col-span-2 lg:row-span-3 md:col-span-2 h-full flex flex-col gap-4'>
+        <div className='flex-1 h-full'>
           <Profile />
         </div>
-        <div>
+        <div className='w-full'>
           <Github />
         </div>
       </div>
 
       {/* Education and Notable Project */}
-      <div className='lg:col-span-2 md:col-span-1'>
+      <div className='lg:col-span-2 md:col-span-1 h-full'>
         <Education />
       </div>
-      <div className='lg:col-span-2 md:col-span-1'>
+      <div className='lg:col-span-2 md:col-span-1 h-full'>
         <Project />
       </div>
 
       {/* Experience */}
-      <div className='lg:col-span-3 lg:row-span-2 md:col-span-full h-full'>
+      <div className='lg:col-span-3 lg:row-span-2 md:col-span-2 h-full'>
         <Experience />
       </div>
 
       {/* Skills */}
-      <div className='lg:col-span-1 lg:row-span-2 md:col-span-full h-full'>
+      <div className='lg:col-span-1 lg:row-span-2 md:col-span-2 h-full'>
         <Skills />
       </div>
     </div>
