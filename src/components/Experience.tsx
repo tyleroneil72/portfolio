7c017c1ca8a17838f34fc54a169de3542a33556b
Tyler O'Neil <tyleroneildev@gmail.com>
2024-12-21 15:30:18 -0500

    Space out experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,19 +7,22 @@ const Experience = () => {
       <h2 className='mb-4 flex items-center gap-2 text-xl font-bold text-indigo-400'>
         <FaBriefcase /> Experience
       </h2>
-      {experience.map((job, index) => (
-        <div key={index} className='mb-2 rounded-lg bg-slate-700 p-2 shadow-md'>
-          <h3 className='font-bold'>
-            {job.role} - {job.company}
-          </h3>
-          <p className='text-sm text-gray-300'>{job.period}</p>
-          <ul className='mt-2 list-disc pl-5 text-sm text-gray-300'>
-            {job.details.map((detail, idx) => (
-              <li key={idx}>{detail}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+
+      <div className='flex flex-1 flex-col justify-evenly space-y-4 overflow-y-auto'>
+        {experience.map((job, index) => (
+          <div key={index} className='rounded-lg bg-slate-700 p-4 shadow-md'>
+            <h3 className='font-bold'>
+              {job.role} - {job.company}
+            </h3>
+            <p className='mt-1 text-sm text-gray-300'>{job.period}</p>
+            <ul className='mt-2 list-disc pl-5 text-sm text-gray-300'>
+              {job.details.map((detail, idx) => (
+                <li key={idx}>{detail}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
