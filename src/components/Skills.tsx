a611995a27ca15fc5b82bd21e70bfdd79c4f71d5
Tyler O'Neil <tyleroneildev@gmail.com>
2024-12-21 01:08:13 -0500

    Add skills icon

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,9 +1,12 @@
 import skills from "../assets/data/skills.json";
+import { FaCode } from "react-icons/fa";
 
 const Skills = () => {
   return (
     <div className='bg-gray-800 h-full rounded-lg p-6 shadow-md'>
-      <h2 className='text-xl font-bold text-indigo-400 mb-4'>Skills</h2>
+      <h2 className='text-xl font-bold text-indigo-400 mb-4 flex items-center gap-2'>
+        <FaCode /> Skills
+      </h2>
       <div className='flex flex-wrap gap-2'>
         {skills.map((skill, index) => (
           <span
