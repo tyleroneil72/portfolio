8266f990d17cb78751b2927ef1f5a58e25fe94a9
Tyler O'Neil <tyleroneildev@gmail.com>
2024-12-21 01:06:30 -0500

    Add experience icon

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,9 +1,12 @@
 import experience from "../assets/data/experience.json";
+import { FaBriefcase } from "react-icons/fa";
 
 const Experience = () => {
   return (
     <div className='bg-gray-800 rounded-lg p-6 shadow-md flex flex-col h-full'>
-      <h2 className='text-xl font-bold text-indigo-400 mb-4'>Experience</h2>
+      <h2 className='text-xl font-bold text-indigo-400 mb-4 flex items-center gap-2'>
+        <FaBriefcase /> Experience
+      </h2>
       {experience.map((job, index) => (
         <div key={index} className='mb-6'>
           <h3 className='font-bold'>
