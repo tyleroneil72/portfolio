68b8518931eab33b4501b71f79b3b5bbca47f816
Tyler O'Neil <tyleroneildev@gmail.com>
2025-01-21 21:17:36 -0500

    Add Framer Motion

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,6 @@
+import { motion } from 'framer-motion';
 import { FaGithub, FaHome } from 'react-icons/fa';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const routes = [
   { path: '/', icon: <FaHome /> },
@@ -7,20 +8,30 @@ const routes = [
 ];
 
 const NavigationBar = () => {
+  const location = useLocation();
+
   return (
     <nav className='fixed left-1/2 top-2 z-50 w-[80%] max-w-3xl -translate-x-1/2 transform rounded-full bg-gray-800 text-white shadow-md'>
-      <div className='flex justify-around py-2'>
+      <div className='relative flex justify-around py-2'>
         {routes.map(({ path, icon }) => (
           <NavLink
             key={path}
             to={path}
             className={({ isActive }) =>
-              `flex items-center justify-center rounded-full text-lg transition ${
+              `relative flex items-center justify-center text-lg transition ${
                 isActive ? 'text-indigo-400' : 'text-gray-300'
               }`
             }
           >
             {icon}
+            {location.pathname === path && (
+              <motion.div
+                layoutId='underline'
+                className='absolute left-0 right-0 top-5 h-1 rounded-full bg-indigo-400'
+                initial={false}
+                transition={{ type: 'spring', stiffness: 300, damping: 40 }}
+              />
+            )}
           </NavLink>
         ))}
       </div>
